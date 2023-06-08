19abc1c18754930773e55d4749a94f9e308e24c7
gabriella2709 <115476691+gabriella2709@users.noreply.github.com>
2023-06-08 17:30:26 +0200

    comit

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header className='header'>
+        <div className="container">
+        <span className="logo">LOGO</span>
+        <nav>
+          <ul className="nav-list">
+            <li className="nav-list-item">
+              <a href="#hero">Jopa</a>
+            </li>
+            <li className="nav-list-item">
+              <a href="#about">About</a>
+            </li>
+            <li className="nav-list-item">
+              <a href="#contacts">Contacts</a>
+            </li>
+          </ul>
+        </nav>
+        <address>
+          <ul className="address-list">
+            <li className="address-list-item">
+              <a href="tel:+1111111111">+1 111 111 111</a>
+            </li>
+            <li className="address-list-item">
+              <a href="mailto:afa@gmail.com">abobus@gmail.com</a>
+            </li>
+          </ul>
+        </address>
+        </div>
+      </header>
+      <main>
+        <section id="hero" className='section hero'>
+          <div className="container">
+            <h1 className="main-text">AMABATAKUM</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur placeat saepe, omnis illum adipisci est aperiam expedita recusandae perferendis porro labore provident vitae dolorum nisi dignissimos! Aperiam, excepturi quas ducimus distinctio unde necessitatibus culpa tempore consequatur soluta sapiente velit earum dolor repudiandae, quis tenetur? Asperiores nisi quisquam, dolorum quod optio quae voluptatibus ea incidunt cum et? Eveniet modi deserunt necessitatibus, soluta beatae aspernatur ab corrupti sed consequatur tenetur adipisci fuga minima accusamus sequi illo dolore! Numquam sed, itaque omnis necessitatibus assumenda quod, fugit dolores quam cum quasi reiciendis aperiam vel qui! In incidunt cum, sed possimus illo cupiditate nam maiores!</p>
+            <button type='button' className='baton'>Baton</button>
+          </div>
+        </section>
+        <section id="about" className='section about'>
+          <div className="container">
+            <h2 className='baton2'>About US</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur alias in labore! Quae in non repellendus, facere recusandae repudiandae quia sit accusamus nobis inventore distinctio maxime sapiente! Consequuntur sed quidem quis quisquam! Distinctio, recusandae voluptatum in optio placeat ipsam officiis. Aliquam, possimus natus. Quam sint consectetur maiores, soluta impedit id recusandae veniam hic corrupti quis, dolores aperiam voluptatem similique unde nam consequuntur. Quia eius magni iusto! Incidunt similique, iste itaque debitis distinctio error quam quisquam deserunt. Hic quasi provident expedita possimus, maiores veniam! Accusantium ipsam magni nostrum quas perspiciatis, ex dicta totam in ab dolore architecto possimus incidunt ipsum exercitationem quidem unde sequi omnis nulla nihil sed repudiandae quos ratione. Aliquam, asperiores alias omnis quam dolorem pariatur. Est corporis quo maiores blanditiis porro tempora ullam odit nobis quisquam vero. Praesentium libero eveniet quas commodi dicta dolorum facere quos quasi laudantium reprehenderit voluptatem ipsam temporibus voluptate excepturi quod repellendus, cupiditate nulla doloremque? Iusto obcaecati repellendus magni sed, reprehenderit iure hic doloribus unde vero, eum reiciendis ad. Cumque optio distinctio reprehenderit veniam modi? Quaerat iusto repellat pariatur adipisci nihil voluptatem incidunt inventore obcaecati, et ea porro neque cumque maxime suscipit magni atque nostrum accusamus minima molestiae corporis quod! Magnam totam adipisci est, deleniti perspiciatis modi nam quam recusandae ea repellat sed quaerat suscipit. Officia optio, praesentium commodi laudantium, debitis accusamus dicta facere vitae sit cumque nesciunt! Perspiciatis dolorum voluptate tempora ex inventore necessitatibus facere odio laborum quibusdam delectus impedit nam optio, aperiam enim. Doloribus reprehenderit eius tempora. Et minus sunt accusantium veritatis deserunt qui officia, sint neque quo doloribus non dolorem vitae tenetur quisquam, quod aliquam suscipit fugiat possimus facilis id voluptates? Voluptas vitae nihil assumenda qui nulla cum debitis error eveniet alias unde! Eos ab magnam ullam consequatur alias enim doloribus nam, voluptatum explicabo, nulla accusamus fuga pariatur eum saepe. Officia culpa quo facere, perspiciatis ipsam nemo inventore? Dolores sed animi debitis? Vitae ut voluptatibus sit sunt ea, impedit odit, praesentium minima, repellat veniam magnam placeat corporis ullam eaque a quibusdam accusamus architecto necessitatibus non blanditiis suscipit doloribus optio voluptatem! Recusandae, placeat eius architecto tempore, quis quam consequatur fugit tenetur alias a, animi dolores iure. Repudiandae, quas earum blanditiis vel minima temporibus mollitia officiis! Vel laudantium quasi fugit iste magni reprehenderit nam odit, vero maiores, magnam ipsa illum animi voluptas porro consequuntur nostrum distinctio ad hic consectetur impedit molestiae! Non doloremque consectetur dolores quidem accusamus. Quisquam amet quis aut voluptatum quam ad recusandae minima similique architecto iusto non magni praesentium pariatur ex odio exercitationem, dicta consequatur quasi illo alias necessitatibus nulla eligendi. Architecto officiis error incidunt perspiciatis suscipit est aliquam aperiam, enim nam adipisci reiciendis! Labore debitis facilis atque, nesciunt ducimus corporis libero, itaque quisquam dolore pariatur quas corrupti animi voluptatibus! Voluptates ipsum minus veniam quos laudantium excepturi dignissimos pariatur eligendi cupiditate eveniet, temporibus asperiores animi facilis provident iusto ipsam numquam sed obcaecati. Explicabo, incidunt saepe earum nulla neque dolorem modi dolor omnis distinctio similique aut, repellendus minus voluptate. Esse, nesciunt nemo debitis omnis fuga inventore ratione dolorum officiis natus earum?</p>
+          </div>
+        </section>
+      </main>
+      
+    </>
+  )
+}
+
+export default App
